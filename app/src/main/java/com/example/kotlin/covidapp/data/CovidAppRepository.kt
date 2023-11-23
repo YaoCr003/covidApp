49068fe25db29a960c91fce8069da4060c3b468c
Yael Cortés Rubio <A01275893@tec.mx>
2023-11-23 14:07:01 -0600

    Prueba 2 api

--- app/src/main/java/com/example/kotlin/covidapp/data/CovidAppRepository.kt
+++ app/src/main/java/com/example/kotlin/covidapp/data/CovidAppRepository.kt
@@ -1,15 +1,15 @@
 package com.example.kotlin.covidapp.data
 
 import com.example.kotlin.covidapp.data.network.CovidAPIService
+import com.example.kotlin.covidapp.data.network.NetworkModuleDI
 import com.example.kotlin.covidapp.data.network.model.CovidAppObject
 import com.example.kotlin.covidapp.data.network.model.covidapp.CovidApp
 
 class CovidAppRepository {
     private lateinit var api: CovidAPIService
 
-    suspend fun getPokemonList(limit:String):CovidAppObject?{
-
-        //todo: Inicializar variable api
+    suspend fun getCovidList(limit:String):CovidAppObject?{
+        api = NetworkModuleDI()
         return try{
             api.getPokemonList(limit)
         }catch (e:java.lang.Exception){
@@ -19,8 +19,8 @@ class CovidAppRepository {
 
     }
 
-    suspend fun getPokemonInfo(nCountry:String): CovidApp? {
-        //todo: Inicializar variable api
+    suspend fun getCovidInfo(nCountry:String): CovidApp? {
+        api = NetworkModuleDI()
         return try{
             api.getPokemonInfo(nCountry)
         }catch (e:java.lang.Exception){
---
+++ app/src/main/java/com/example/kotlin/covidapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.kotlin.covidapp.utils
+
+object Constants {
+    const val BASE_URL = "https://api.api-ninjas.com/v1/covid19"
+    const val MAX_INFO_COUNTRY = "canada"
+}
